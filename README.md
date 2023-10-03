4517c5f37275ebfac35fe5717ef5cb87eceed31d
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-10-03 06:18:49 -0300

    atualização

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# previsão de futuras regiões EURUSD
+Aqui está um exemplo de um README básico para seu código:
+
+```markdown
+# Previsão de Preços do EUR/USD usando Regressão Linear
+
+Este projeto tem como objetivo prever os preços máximos, mínimos, de fechamento e o preço médio do par de moedas EUR/USD para o próximo dia usando regressão linear.
+
+## Requisitos
+
+Certifique-se de ter as seguintes bibliotecas instaladas:
+
+- pandas
+- scikit-learn
+- datetime
+- numpy
+- pandas_datareader
+- yfinance
+
+Você pode instalar as bibliotecas usando o comando `pip install`, por exemplo:
+
+```
+pip install pandas
+pip install scikit-learn
+pip install pandas_datareader
+pip install yfinance
+```
+
+## Como Usar
+
+1. Importe as bibliotecas necessárias:
+
+```python
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
+import numpy as np
+import pandas_datareader.data as pdr
+import yfinance as yf
+```
+
+2. Defina a função `prever_proximo_dia()` para fazer a previsão dos preços:
+
+```python
+# Função para prever os preços do próximo dia
+def prever_proximo_dia(eurusd):
+    # Implementação da função...
+    return previsao_max, previsao_min, previsao_Close, previsao_preco_medio
+```
+
+3. Defina o ticker do ativo que você deseja importar e obtenha a data atual:
+
+```python
+# Defina o ticker do ativo que você deseja importar
+ticker = "EURUSD=X"  # Exemplo: Apple Inc.
+
+# Obtenha a data atual
+data_atual = datetime.today().strftime('%Y-%m-%d')
+```
+
+4. Importe os dados do Yahoo Finance e chame a função `prever_proximo_dia()`:
+
+```python
+# Use a função download() para importar os dados
+eurusd = yf.download(ticker, start="2021-01-01", end=data_atual)
+
+# Chamar a função para fazer a previsão
+previsao_max, previsao_min, previsao_Close, previsao_preco_medio = prever_proximo_dia(eurusd)
+```
+
+5. Imprima os resultados das previsões:
+
+```python
+# Imprimir os resultados das previsões
+print(f"Previsão da máxima do próximo dia: {previsao_max:.2f}")
+print(f"Previsão da mínima do próximo dia: {previsao_min:.2f}")
+print(f"Previsão do fechamento do próximo dia: {previsao_Close:.2f}")
+print(f"Previsão do preço médio do próximo dia: {previsao_preco_medio:.2f}")
+```
+
+Certifique-se de ajustar o código e as instruções de acordo com suas necessidades específicas.
+
+## Limitações e Melhorias Futuras
+
+- Este projeto usa uma abordagem simples de regressão linear para prever os preços do próximo dia. Outros modelos mais avançados, como redes neurais, podem ser explorados para melhorar a precisão das previsões.
+- A previsão é feita com base nos dados históricos do par de moedas EUR/USD. O desempenho do modelo pode variar dependendo das condições de mercado e de outros fatores externos.
+- Este projeto pode ser expandido para prever preços para períodos mais longos ou para outros ativos financeiros.
+```
+
+Este README básico fornece uma descrição geral do seu projeto, instruções sobre como usá-lo e algumas informações sobre limitações e melhorias futuras. Você pode personalizá-lo de acordo com as necessidades e detalhes específicos do seu projeto.
  
--- eurusd.py
+++ eurusd.py
@@ -1,65 +1,74 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from datetime import datetime
-from tvDatafeed import TvDatafeed, Interval
 import numpy as numpy
+import pandas_datareader.data as pdr
+import yfinance as yf
+
 
 def prever_proximo_dia(eurusd):
     # Transformar os dados em um DataFrame
     eurusd = pd.DataFrame(eurusd)
     
     # Calcular o preço de fechamento médio
-    eurusd['Preco Medio'] = (eurusd['high'] + eurusd['low']) / 2
+    eurusd['Adj Close'] = (eurusd['High'] + eurusd['Low']) / 2
 
     # Criar colunas com os preços do dia seguinte (shift(-1))
-    eurusd['Proximo dia max'] = eurusd['high'].shift(-1)
-    eurusd['Proximo dia min'] = eurusd['low'].shift(-1)
-    eurusd['Proximo dia close'] = eurusd['close'].shift(-1)
-    eurusd['Proximo dia Preco Medio'] = eurusd['Preco Medio'].shift(-1)
+    eurusd['Proximo dia max'] = eurusd['High'].shift(-1)
+    eurusd['Proximo dia min'] = eurusd['Low'].shift(-1)
+    eurusd['Proximo dia Close'] = eurusd['Close'].shift(-1)
+    eurusd['Proximo dia Preco Medio'] = eurusd['Adj Close'].shift(-1)
 
     eurusd = eurusd.dropna()
 
     # Substituir NaN por 0
     eurusd.fillna(0, inplace=True)
 
-    # Dividir os dados em variáveis de entrada (X) e saída (y) para max, min, close e preco medio
-    X = eurusd[['Preco Medio']]
+    # Dividir os dados em variáveis de entrada (X) e saída (y) para max, min, Close e preco medio
+    X = eurusd[['Adj Close']]
     y_max = eurusd[['Proximo dia max']]
     y_min = eurusd[['Proximo dia min']]
-    y_close = eurusd[['Proximo dia close']]
+    y_Close = eurusd[['Proximo dia Close']]
     y_preco_medio = eurusd[['Proximo dia Preco Medio']]
 
-    # Criar modelos de regressão linear para max, min, close e preco medio
+    # Criar modelos de regressão linear para max, min, Close e preco medio
     modelo_max = LinearRegression()
     modelo_min = LinearRegression()
-    modelo_close = LinearRegression()
+    modelo_Close = LinearRegression()
     modelo_preco_medio = LinearRegression()
 
     # Treinar os modelos
     modelo_max.fit(X, y_max)
     modelo_min.fit(X, y_min)
-    modelo_close.fit(X, y_close)
+    modelo_Close.fit(X, y_Close)
     modelo_preco_medio.fit(X, y_preco_medio)
 
-    # Fazer as previsões para max, min, close e preco medio do próximo dia
-    previsao_max = modelo_max.predict([[eurusd['close'].iloc[-1]]])[0][0]
-    previsao_min = modelo_min.predict([[eurusd['close'].iloc[-1]]])[0][0]
-    previsao_close = modelo_close.predict([[eurusd['close'].iloc[-1]]])[0][0]
-    previsao_preco_medio = modelo_preco_medio.predict([[eurusd['close'].iloc[-1]]])[0][0]
+    # Fazer as previsões para max, min, Close e preco medio do próximo dia
+    previsao_max = modelo_max.predict([[eurusd['Close'].iloc[-1]]])[0][0]
+    previsao_min = modelo_min.predict([[eurusd['Close'].iloc[-1]]])[0][0]
+    previsao_Close = modelo_Close.predict([[eurusd['Close'].iloc[-1]]])[0][0]
+    previsao_preco_medio = modelo_preco_medio.predict([[eurusd['Close'].iloc[-1]]])[0][0]
 
-    return previsao_max, previsao_min, previsao_close, previsao_preco_medio
+    return previsao_max, previsao_min, previsao_Close, previsao_preco_medio
 
-# Exemplo de uso da função
-tv = TvDatafeed()
-# Baixar dados históricos
-eurusd = tv.get_hist(symbol='eurusd', exchange='pepperstone', interval=Interval.in_daily, n_bars=10000)
+
+
+# Defina o ticker do ativo que você deseja importar
+ticker = "EURUSD=X"  # Exemplo: Apple Inc.
+
+# Obtenha a data atual
+data_atual = datetime.today().strftime('%Y-%m-%d')
+
+# Use a função download() para importar os dados
+eurusd = yf.download(ticker, start="2021-01-01", end=data_atual)
+
 
 # Chamar a função para fazer a previsão
-previsao_max, previsao_min, previsao_close, previsao_preco_medio = prever_proximo_dia(eurusd)
+previsao_max, previsao_min, previsao_Close, previsao_preco_medio = prever_proximo_dia(eurusd)
 
 # Imprimir os resultados das previsões
 print(f"Previsão da máxima do próximo dia: {previsao_max:.2f}")
 print(f"Previsão da mínima do próximo dia: {previsao_min:.2f}")
-print(f"Previsão do fechamento do próximo dia: {previsao_close:.2f}")
+print(f"Previsão do fechamento do próximo dia: {previsao_Close:.2f}")
 print(f"Previsão do preço médio do próximo dia: {previsao_preco_medio:.2f}")
 
